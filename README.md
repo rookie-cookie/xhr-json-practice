156204abffd08f8f62cdac799d23c71e5c2b9ff8
Jona Reyes <jonamreyes@gmail.com>
2021-02-18 10:58:17 -0500

    axios basic use

--- README.md
+++ README.md
@@ -5,3 +5,8 @@ Resources:
 https://www.youtube.com/watch?v=4K33w-0-p2c
 
 https://reqres.in/
+
+https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API/Using_Fetch
+
+https://github.com/axios/axios
+
--- axios.js
+++ axios.js
@@ -1,9 +1,28 @@
 const getBtn = document.getElementById('get-btn');
 const postBtn = document.getElementById('post-btn');
 
-const getData = () => {};
+const getData = () => {
+	axios.get('https://reqres.in/api/users').then(response => {
+		console.log(response);
+	});
+};
 
-const sendData = () => {};
+const sendData = () => {
+	axios.post('https://reqres.in/api/register', {
+		email: "eve.holt@reqres.in",
+    	//password: "pistol"
+    }, {
+
+    	// headers: {
+    	// 	'Content-Type': 'application/json';
+    	// }
+	})
+	.then(response => {
+		console.log(response);
+	}).catch(err => {
+		console.log(err, err.response); //axios automatically throws an error if the response have error status code ie >= 400 
+	})
+};
 
 getBtn.addEventListener('click', getData);
 postBtn.addEventListener('click', sendData);
